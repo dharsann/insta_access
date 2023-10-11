14928ae34dc4f0fce7a39d40a13b0b34ebddf5b8
dharsann <131436118+dharsann@users.noreply.github.com>
2023-10-11 19:48:29 +0530

    Update iam.py

--- iam.py
+++ iam.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify
 import requests
 import os
 import logging
-from functools import lru_cache
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -11,43 +10,37 @@ app = Flask(__name__)
 load_dotenv()
 
 # Access the access_token from the environment
-access_token = os.getenv('')
+access_token = os.getenv('ACCESS_TOKEN')
 
 # Add logging
 logging.basicConfig(filename='app.log', level=logging.INFO)
 
-# Use lru_cache as a simple caching mechanism
-@lru_cache(maxsize=100)
-def get_profile_data_from_api():
+@app.route('/')
+def index():
+    app.logger.info('Redirection is Successful')
+    return 'Redirection is Successful'
+
+@app.route('/profile_data')
+def get_profile_data():
     # Get user ID
     response = requests.get(f'https://graph.instagram.com/me?fields=id&access_token={access_token}')
+    
     if response.status_code == 200:
         user_id = response.json()['id']
     else:
         app.logger.error('Failed to get user ID from Instagram API')
-        return None
+        return 'Error: Could not retrieve user ID'
 
     # Get profile data
     response = requests.get(f'https://graph.instagram.com/{user_id}?fields=id,username,account_type,media_count,followers_count&access_token={access_token}')
 
     if response.status_code == 200:
         profile_data = response.json()
-        return profile_data
-    else:
-        app.logger.error('Failed to retrieve profile data from Instagram API')
-        return None
-
-@app.route('/')
-def index():
-    return 'Redirection is Successful'
-
-@app.route('/profile_data')
-def get_profile_data():
-    profile_data = get_profile_data_from_api()
-    if profile_data is not None:
+        app.logger.info('Profile data retrieved successfully')
         return jsonify(profile_data)
     else:
+        app.logger.error('Failed to retrieve profile data from Instagram API')
         return 'Error: Could not retrieve profile data'
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1",port=8080)
+    app.run()
